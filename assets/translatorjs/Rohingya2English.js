c4338b603a67ec2ce647d9036c627d446ccd6f36
Rohingya Translator <translator.rohingya@gmail.com>
2024-07-10 09:05:30 +0300

    Update Rohingya2English.js

--- assets/translatorjs/Rohingya2English.js
+++ assets/translatorjs/Rohingya2English.js
@@ -1,4 +1,3 @@
-<script>
 function translateText() {
 let inputText = document.getElementById('inputText').value;
 let outputText = translate(inputText); // Replace with your translation function
@@ -14050,5 +14049,3 @@ let translatedText = words.map(word => translations[word.toLowerCase()] || word)
 
 return translatedText;
 }
-
-</script>
