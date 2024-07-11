a4e8ba55ca6da37ae22b62306962fe6dfaf4a5c0
Rohingya Translator <translator.rohingya@gmail.com>
2024-07-11 05:41:03 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -111,3 +111,16 @@ sr.reveal(`.share__img, .send__content`, {
 sr.reveal(`.share__data, .send__img`, {
     origin: 'right'
 })
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+    social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+    social_panel_container.classList.remove('visible')
+});
