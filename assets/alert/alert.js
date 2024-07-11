94bed02ddc1d1b195dd4782c4aa61bcbdac82f97
Rohingya Translator <translator.rohingya@gmail.com>
2024-07-11 22:02:23 +0300

    Update alert.js

--- assets/alert/alert.js
+++ assets/alert/alert.js
@@ -1,1 +1,16 @@
+    document.addEventListener("DOMContentLoaded", function() {
+        var modal = document.getElementById("modal");
+        var closeButton = document.getElementById("close-button");
 
+        modal.style.display = "flex";
+
+        closeButton.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    });
